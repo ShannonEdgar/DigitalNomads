658684b80d9b819a99f521a58d8bd732d5e639f8
yelena guliyeva <gulbadam@gulbadam.com>
2018-03-10 11:38:28 -0800

    delete old

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -292,3 +292,31 @@ function createMarker(place) {
         infowindow.open(map, this);
     });
 }
+$(function() {
+    var options = {
+        map: '.map_canvas',
+    };
+
+    $('#geocomplete')
+        .geocomplete(options)
+        .bind('geocode:result', function(event, result) {
+            $.log('Result: ' + result.formatted_address);
+        })
+        .bind('geocode:error', function(event, status) {
+            $.log('ERROR: ' + status);
+        })
+        .bind('geocode:multiple', function(event, results) {
+            $.log('Multiple: ' + results.length + ' results found');
+        });
+
+    $('#find').click(function() {
+        $('#geocomplete').trigger('geocode');
+    });
+
+    $('#examples a').click(function() {
+        $('#geocomplete')
+            .val($(this).text())
+            .trigger('geocode');
+        return false;
+    });
+});
